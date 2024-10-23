85805508635d01700d0adeb2974b4a5cdf88aa10
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-23 22:34:33 +0800

    refactor: optimized imports

--- app/src/main/java/com/example/kotlin_compose_myinfo/ui/home/view/HomeView.kt
+++ app/src/main/java/com/example/kotlin_compose_myinfo/ui/home/view/HomeView.kt
@@ -1,20 +1,13 @@
 package com.example.kotlin_compose_myinfo.ui.home.view
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
