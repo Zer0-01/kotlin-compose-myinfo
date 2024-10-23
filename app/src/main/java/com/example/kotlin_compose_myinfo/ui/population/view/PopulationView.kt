c7f48216967a9e867f580dff3fb0c6a69adcfc5e
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-23 22:53:53 +0800

    feat: create top app bar for the population view

--- app/src/main/java/com/example/kotlin_compose_myinfo/ui/population/view/PopulationView.kt
+++ app/src/main/java/com/example/kotlin_compose_myinfo/ui/population/view/PopulationView.kt
@@ -1,8 +1,40 @@
 package com.example.kotlin_compose_myinfo.ui.population.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.kotlin_compose_myinfo.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PopulationView() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                            contentDescription = null
+                        )
+                    }
+                },
+                title = { Text(text = stringResource(R.string.population_population)) }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
 
+        }
+    }
 }
