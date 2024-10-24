2e19e5cf34be51c43542f6faf03c176c968eaf91
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-24 23:34:14 +0800

    feat: create population table (malaysia) and (state) for the population page

--- app/src/main/java/com/example/kotlin_compose_myinfo/ui/population/view/PopulationView.kt
+++ app/src/main/java/com/example/kotlin_compose_myinfo/ui/population/view/PopulationView.kt
@@ -1,9 +1,14 @@
 package com.example.kotlin_compose_myinfo.ui.population.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,6 +18,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.kotlin_compose_myinfo.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,8 +39,88 @@ fun PopulationView() {
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.population_population_table_malaysia)
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                }
 
+            }
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.population_population_table_state)
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(text = "2024")
+                        Text(text = "12345678")
+                    }
+                }
+
+            }
         }
     }
 }
